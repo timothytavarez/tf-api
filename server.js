2778a9c75f7f3c5cc2814fcc3f9a1f74ce65ab51
Timothy Tavarez <timothy.tavarez@microsoft.com>
2018-10-15 10:28:34 -0600

    Add package.json. Add azure storage.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+// const azure = require('azure-storage');
 
 const server = Hapi.server({
     port: '3000',
@@ -9,8 +10,24 @@ const server = Hapi.server({
 
 const init = async () => {
     await server.start();
-    console.log(`Server started on host ${server.port} via port ${server.port}`);
-}
+    console.log(`Server running on ${server.info.uri}`);
+};
+
+server.route({
+    method: 'GET',
+    path: '/states',
+    handler: function (request, h) {
+        return 'Success!';
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/states/new',
+    handler: function (request, h) {
+        
+    }
+});
 
 process.on('UnhandledRejection', (err) => {
     console.error(err);
